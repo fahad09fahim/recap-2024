53d862fde044987e3688e717cf02ddcce23aa732
fahad09fahim <anik.fahim07@gmail.com>
2024-10-17 00:47:37 +0600

    header file complete with html css

--- phone hunter/index.js
+++ phone hunter/index.js
@@ -1,1 +1,15 @@
-console.log("Hello client");
+// https://openapi.programming-hero.com/api/phones?search=iphone
+
+// https://openapi.programming-hero.com/api/phone/${id}
+
+//get phone data
+const phoneData = async () => {
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/phones?search=iphone"
+  );
+  const phoneData = await response.json();
+  const phones = phoneData.data;
+  console.log(phones);
+};
+
+phoneData();
